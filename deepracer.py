7fcc2c06de4525239087da64138d6ed486bde464
Anubhav De <anubhavde2002@gmail.com>
2022-08-04 15:45:42 +0530

    Update deepracer.py

--- deepracer.py
+++ deepracer.py
@@ -2,38 +2,49 @@ def reward_function(params):
     # Example of penalize steering, which helps mitigate zig-zag behaviors
 
     # Read input parameters
+    all_wheels_on_track = params['all_wheels_on_track']
     distance_from_center = params['distance_from_center']
     track_width = params['track_width']
     abs_steering = abs(params['steering_angle']) # Only need the absolute steering angle
-    all_wheels_on_track = params['all_wheels_on_track']
     speed = params['speed']
-
+    
+    # threshold for speed to complete race faster
     SPEED_THRESHOLD = 1.0
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
+    
+    # Give a very low reward by default
+    reward = 1e-3
 
+    # Give a high reward if no wheels go off the track and
+    # the agent is somewhere in between the track borders
+    if all_wheels_on_track and (0.5*track_width - distance_from_center) >= 0.05:
+        reward = 1.0
+        
     # Calculate 3 marks that are farther and father away from the center line
     marker_1 = 0.1 * track_width
-    marker_2 = 0.35 * track_width
+    marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
 
-    # Penalize reward heavily if car is out of track
-    if (distance_from_center > marker_3) or (not all_wheels_on_track):
-        reward = 1e-3
+    # Give higher reward if the car is closer to center line and vice versa
+    if distance_from_center <= marker_1:
+        reward = 1.0
+    elif distance_from_center <= marker_2:
+        reward = 0.5
+    elif distance_from_center <= marker_3:
+        reward = 0.1
+    else:
+        reward = 1e-3  # likely crashed/ close to off track
 
+    # Steering penality threshold, change the number based on your action space setting
+    ABS_STEERING_THRESHOLD = 15 
+    
     # Penalize reward if the car is steering too much
     if abs_steering > ABS_STEERING_THRESHOLD:
         reward *= 0.8
-
-    if all_wheels_on_track and distance_from_center <= marker_2 and speed > SPEED_THRESHOLD:
-        reward = 1.0
-    elif all_wheels_on_track and distance_from_center > marker_2 and distance_from_center <= marker_3 and speed <= SPEED_THRESHOLD:
-        reward = 0.3
-    elif all_wheels_on_track and distance_from_center > marker_2 and distance_from_center <= marker_3 and speed > SPEED_THRESHOLD:
-        reward = 1.0
-    # likely crashed/ close to off track
-    else:
-        reward = 1e-3
-
         
+    # Ensure car is at maximum speed possible
+    if speed < SPEED_THRESHOLD:
+        reward += 0.1
+    else:
+        reward += 1.0
+    
     return float(reward)
